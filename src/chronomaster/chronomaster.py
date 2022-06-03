58a75521411401b9ce69966a36719b7212f941fc
yenonn <yenonn@gmail.com>
2022-06-03 21:21:15 +0800

    chronomaster in progress

--- src/chronomaster/chronomaster.py
+++ src/chronomaster/chronomaster.py
@@ -5,16 +5,38 @@ import requests
 
 
 class Chronomaster(object):
-    url = "http://skywalker-proxy:5000"
+    url = "http://skywalker-proxy:5000/execute"
 
     def __init__(self):
         self.env = os.getenv("Environment")
         self.sched = BlockingScheduler()
         self.job_requests = JobRequest(self.env).requests
-        self.schedules = Schedule(self.env).schedules
 
-    def task(self):
-        requests.post(self.url)
+    def _trigger(self, data):
+        requests.post(self.url, data=data, headers={"Content-Type": "Application/json"})
+
+    def jobs(self):
+        for config, job in self.job_requests.items():
+            print(f"Fetching job - {config}")
+            yield job
+
+    def add_jobs(self):
+        for job in self.jobs():
+            cron = job.get("cron")
+            job.pop("environment")
+            self.sched.add_job(self._trigger, "interval", [job], seconds=5)
+
+    def start(self):
+        self.sched.start()
+
+    def stop(self):
+        self.sched.shutdown()
+
+    def pause(self):
+        self.sched.pause()
+
+    def resume(self):
+        self.sched.resume()
 
 
 class JobRequest(object):
@@ -24,18 +46,16 @@ class JobRequest(object):
         self.requests = self.job_requests()
 
     def job_requests(self):
-        job_requests = []
-        for config in self.schedules:
-            for key in config.keys():
-                config_file = key
-                job_name = str(config_file).replace("-config.json", "")
-                job_requests.append(
-                    {
-                        "job_name": job_name,
-                        "python-codes-config": config_file,
-                        "python-codes-args": {},
-                    }
-                )
+        job_requests = {}
+        for config, schedule in self.schedules.items():
+            job_name = str(config).replace("-config.json", "")
+            job = {
+                "job_name": job_name,
+                "python-codes-config": config,
+                "python-codes-args": {},
+            }
+            job_requests[config] = {**job, **schedule}
+
         return job_requests
 
 
@@ -51,11 +71,11 @@ class Schedule(object):
         return config_files
 
     def find_schedules(self):
-        schedules = []
+        schedules = {}
         for file in self.function_configs:
             with open(file) as jsonfile:
                 data = json.load(jsonfile)
                 for sched in data.get("schedule"):
                     if sched.get("environment") == self.env:
-                        schedules.append({file: sched})
+                        schedules[file] = sched
         return schedules
