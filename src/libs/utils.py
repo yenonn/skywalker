ceb81b39b042def14e4311a24a7ea1f596187458
yenonn <yenonn@gmail.com>
2022-08-28 07:33:28 +0800

    utility codes

--- src/libs/utils.py
+++ src/libs/utils.py
@@ -1,6 +1,10 @@
 import os
 import json
 import time
+import datetime
+import dateutil.parser
+import dateutil.tz
+import pytz
 from importlib import import_module
 from logger import LoggerFactory
 from envparse import env
@@ -40,8 +44,8 @@ def load_module(handler: str):
         try:
             module = import_module(pathname)
             caller = getattr(module, name)
-        except ImportError as e:
-            logger.error(f"Importing module error: {e}")
+        except (ImportError, AttributeError, ValueError) as e:
+            logger.error(f"Importing module error {handler}: {e}")
     return caller
 
 
@@ -81,3 +85,81 @@ def config(key: str):
         return env(key)
     if os.getenv(key):
         return os.getenv(key)
+
+
+def ts_to_dt(timestamp):
+    if isinstance(timestamp, datetime.datetime):
+        return timestamp
+    dt = dateutil.parser.parse(timestamp)
+    # Implicitly convert local timestamps to UTC
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=pytz.utc)
+    return dt
+
+
+def dt_to_ts(dt):
+    if not isinstance(dt, datetime.datetime):
+        print("Expected datetime, got %s" % (type(dt)))
+        return dt
+    ts = dt.isoformat()
+    # Round microseconds to milliseconds
+    if dt.tzinfo is None:
+        # Implicitly convert local times to UTC
+        return ts + "Z"
+    # isoformat() uses microsecond accuracy and timezone offsets
+    # but we should try to use millisecond accuracy and Z to indicate UTC
+    return ts.replace("000+00:00", "Z").replace("+00:00", "Z")
+
+
+def ts_to_dt_with_format(timestamp, ts_format):
+    if isinstance(timestamp, datetime.datetime):
+        return timestamp
+    dt = datetime.datetime.strptime(timestamp, ts_format)
+    # Implicitly convert local timestamps to UTC
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=dateutil.tz.tzutc())
+    return dt
+
+
+def dt_to_ts_with_format(dt, ts_format):
+    if not isinstance(dt, datetime.datetime):
+        print("Expected datetime, got %s" % (type(dt)))
+        return dt
+    ts = dt.strftime(ts_format)
+    return ts
+
+
+def ts_now():
+    return datetime.datetime.utcnow().replace(tzinfo=dateutil.tz.tzutc())
+
+
+def ts_utc_to_tz(ts, tz_name):
+    """Convert utc time to local time."""
+    return ts.astimezone(dateutil.tz.gettz(tz_name))
+
+
+def inc_ts(timestamp, milliseconds=1):
+    """Increment a timestamp by milliseconds."""
+    dt = ts_to_dt(timestamp)
+    dt += datetime.timedelta(milliseconds=milliseconds)
+    return dt_to_ts(dt)
+
+
+def pretty_ts(timestamp, tz=True, ts_format=None):
+    """Pretty-format the given timestamp (to be printed or logged hereafter).
+    If tz, the timestamp will be converted to local time.
+    Format: YYYY-MM-DD HH:MM TZ"""
+    dt = timestamp
+    if not isinstance(timestamp, datetime.datetime):
+        dt = ts_to_dt(timestamp)
+    if tz:
+        dt = dt.astimezone(dateutil.tz.tzlocal())
+    if ts_format is None:
+        return dt.strftime("%Y-%m-%d %H:%M %Z")
+    else:
+        return dt.strftime(ts_format)
+
+
+def ts_add(ts, td):
+    """Allows a timedelta (td) add operation on a string timestamp (ts)"""
+    return dt_to_ts(ts_to_dt(ts) + td)
